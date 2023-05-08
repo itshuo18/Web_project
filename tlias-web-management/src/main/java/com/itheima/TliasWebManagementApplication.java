931d96c8200bc192139f71380ab7e151274e0480
itshuo18 <itshuo18@itcast.cn>
2023-05-08 22:49:54 +0800

    变更

--- tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
+++ tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
@@ -3,8 +3,10 @@ package com.itheima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @ServletComponentScan
+@ComponentScan({"com.example","com.itheima"})
 @SpringBootApplication
 public class TliasWebManagementApplication {
 
